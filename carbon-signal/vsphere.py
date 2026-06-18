fe1de93cd1f0bbfafd8c69fcf22106fca3dc867d
kevinAuberson <kevin.auberson@heig-vd.ch>
2026-06-18 14:49:34 +0200

    fix: vsphere pyVim version

--- carbon-signal/vsphere.py
+++ carbon-signal/vsphere.py
@@ -12,7 +12,7 @@ import os
 import ssl
 
 from cache import cache
-from pyVim.connect import Disconnect, SmartConnect
+from pyvim.connect import Disconnect, SmartConnect
 from pyVmomi import vim
 
 
